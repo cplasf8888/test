c1d89ec04bbbfa6adc1551f16b1bf175222c5040
cplasf <1375372555@qq.com>
2022-07-14 11:59:56 +0000

    update src/main/java/com/team/doem2.java. 更改

--- src/main/java/com/team/doem2.java
+++ src/main/java/com/team/doem2.java
@@ -6,7 +6,7 @@ package com.team;
  * @Description: TODO
  * @Version V1.0
  */
-public class doem {
+public class doem2 {
     public static void main(String[] args) {
         System.out.println("我拉取项目");
     }
