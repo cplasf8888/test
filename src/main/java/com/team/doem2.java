78668e16bece67552078e92a7ce22b3d737fdb04
cplasf <1375372555@qq.com>
2022-07-14 11:58:43 +0000

    add src/main/java/com/team/doem2.java.

---
+++ src/main/java/com/team/doem2.java
@@ -0,0 +1,13 @@
+package com.team;
+
+/**
+ * @Author: wy
+ * @Date: 2022/7/13 19:26
+ * @Description: TODO
+ * @Version V1.0
+ */
+public class doem {
+    public static void main(String[] args) {
+        System.out.println("我拉取项目");
+    }
+}
